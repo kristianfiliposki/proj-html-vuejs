7c95d0fa4193066acac0d69312328a44a6c31c36
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-12-14 13:35:38 +0100

    swipeer problem

--- src/components/AppCarosello.vue
+++ src/components/AppCarosello.vue
@@ -1,5 +1,28 @@
 <script>
 import { store } from '../store';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
 export default {
     data() {
         return {
@@ -17,13 +40,47 @@ export default {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
             </p>
-            <div class="SWIPER">SWIPER</div>
+            <div class="carosello">
+
+                <!-- Slider main container -->
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide"><img src="/public/construction/images/home-logo1-219096700-320x202.png"
+                                alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="/proj-html-vuejs/public/construction/images/home-logo2-219096700-320x202.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="/proj-html-vuejs/public/construction/images/home-logo9-219096700-320x202.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="/proj-html-vuejs/public/construction/images/home-logo10-219096700-320x202.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="/proj-html-vuejs/public/construction/images/home-logo11-219096700-320x202.png" alt="">
+                        </div>
+
+                        ...
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <!-- If we need scrollbar -->
+                    <div class="swiper-scrollbar"></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <style scoped>
-.SWIPER {
-    font-size: 6em;
+.carosello {
     margin: 1em;
 }
 
